6181258dea1c2819f2cfacd090de3c95b90c8efd
Hugo <133796530+hugo-sna@users.noreply.github.com>
2023-10-18 16:55:32 +0200

    Added data support

--- commands.py
+++ commands.py
@@ -2,6 +2,7 @@ import jinja2
 import os
 from pathlib import Path
 from shutil import rmtree
+import json
 
 helpTxt = """Arguments:
     help - Gives commands usage
@@ -9,6 +10,8 @@ helpTxt = """Arguments:
 
 def cmdHelp() -> None:
     print(helpTxt)
+
+projectInfo = {}
 
 def cmdBuild() -> None:
     projectPath: str = os.getcwd()
@@ -18,19 +21,22 @@ def cmdBuild() -> None:
         print('Error: this directory isn\'t a STK project')
         return
 
+    with open(f'{projectPath}/stk.json', 'r') as f:
+        projectInfo = json.loads(f.read())
+
     # Declaring useful variables
     srcDir: str = f'{projectPath}/src'
     buildDir: str = f'{projectPath}/build'
     pageDir: str = '/pages'
-    indexPath: str = f'/index.jinja'
-    componentsDir: str = f'/components'
-    headerPath = f'{componentsDir}/header.jinja'
-    footerPath = f'{componentsDir}/footer.jinja'
+    indexPath: str = '/index.jinja'
+    dataDir: str = f'{srcDir}/data'
+    componentsDir: str = '/components'
+    headerPath: str = f'{componentsDir}/header.jinja'
+    footerPath: str = f'{componentsDir}/footer.jinja'
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(srcDir))
 
     indexTemplate = env.get_template(indexPath)
-
 
     if Path(buildDir).is_dir():
         rmtree(buildDir)
@@ -41,25 +47,34 @@ def cmdBuild() -> None:
     header = env.get_template(headerPath).render()
     footer = env.get_template(footerPath).render()
 
-    print(pageDir)
-
     for file in os.listdir(f'{srcDir}/{pageDir}'):
         if not file.endswith('.jinja'):
             continue
 
         template = env.get_template(f'{pageDir}/{file}')
         
-        print(f'{pageDir}/{file}')
-
         newFileName: str
 
+        # Checks if the file is the index
         if file == 'home.jinja':
             newFileName = 'index.html'
         else:
-            newFileName = file[:len(file) - 6]
+            newFileName = f'{file[:len(file) - 6]}.html'
+        
+        fileData = {}
+        # print(f'{dataDir}/{newFileName[:len(newFileName) - 5]}.json')
+        with open(f'{dataDir}/{file[:len(file) - 6]}.json', 'r') as f:
+            fileData = json.loads(f.read())
         
         with open(f'{buildDir}/{newFileName}', 'w') as f:
-            halfRenderedFile = template.render()
-            renderedFile = indexTemplate.render(header=header, content=halfRenderedFile, footer=footer)
+            halfRenderedFile = template.render(data=fileData['data'])
+            renderedFile = indexTemplate.render(
+                header=header,
+                content=halfRenderedFile,
+                footer=footer, 
+                name=projectInfo['name'],
+                stylesheets=fileData['stylesheets'],
+                rawHeadDatas=fileData['rawHeadDatas']
+            )
             f.write(renderedFile)
 
